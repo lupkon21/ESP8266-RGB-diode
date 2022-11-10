d2d5b19ad0434a8dd6b280965cf53a984eec5cb8
Lukas Protiva <luky.protiva@gmail.com>
2022-11-10 20:06:25 +0100

    Make source code more readable and reusable

--- ESP8266-RGB-diode.ino
+++ ESP8266-RGB-diode.ino
@@ -2,6 +2,10 @@ int r = 5;
 int g = 4;
 int b = 14;
 
+int colors1[3][1] = {{r},{g},{b}};        // red, green, blue
+int colors2[3][2] = {{r,g},{r,b},{g,b}};  // yellow, magenta, cyan
+int colors3[3] = {r,g,b};
+
 void setup() {
   pinMode(r,OUTPUT);
   pinMode(g,OUTPUT);
@@ -9,111 +13,58 @@ void setup() {
 }
 
 void loop() {
-  // red
-  for(int i = 0; i <= 1024; i++) {
-    analogWrite(r,i);
+  // for colors made of one basic color
+  for(int i = 0; i < 3; i++) {
+      diodeCycle(colors1[i],1);
+  }
+
+  // for colors made of two basic colors
+  for(int i = 0; i < 3; i++) {
+      diodeCycle(colors2[i],2);
+  }
+
+  // for colors made of three basic colors - white and black (no light)
+  diodeCycle(colors3,3);
+
+  for(int i = 0; i < 3; i++) {
+    digitalWrite(colors3[i],LOW);
+  }
+  delay(10440);
+}
+
+
+/**
+ * Custom functions
+ */
+
+// function for one smooth diode cycle
+void diodeCycle(int color[],int len) {
+  turnOnDiode(color,len);
+  delay(100);
+  turnOffDiode(color,len);
+  delay(100);
+}
+
+
+// function for smooth turning the diode on
+void turnOnDiode(int color[],int len) {
+  for(int i = 0; i <= 1023; i++) {
+    analogWriteCustom(color,i,len);
     delay(5);
   }
-  delay(100);
+}
 
-  for(int i = 1024; i >= 0; i--) {
-    analogWrite(r,i);
+// function for smooth turning the diode off
+void turnOffDiode(int color[],int len) {
+  for(int i = 1023; i >= 0; i--) {
+    analogWriteCustom(color,i,len);
     delay(5);
   }
-  delay(100);
+}
 
-  // green
-  for(int i = 0; i <= 1024; i++) {
-    analogWrite(g,i);
-    delay(5);
+// wrapper over the build-in analogWrite() function
+void analogWriteCustom(int color[],int value,int len) {
+  for(int i = 0; i < len; i++) {
+    analogWrite(color[i],value);
   }
-  delay(100);
-
-  for(int i = 1024; i >= 0; i--) {
-    analogWrite(g,i);
-    delay(5);
-  }
-  delay(100);
-
-  // blue
-  for(int i = 0; i <= 1024; i++) {
-    analogWrite(b,i);
-    delay(5);
-  }
-  delay(100);
-
-  for(int i = 1024; i >= 0; i--) {
-    analogWrite(b,i);
-    delay(5);
-  }
-  delay(100);
-
-  // yellow
-  for(int i = 0; i <= 1024; i++) {
-    analogWrite(r,i);
-    analogWrite(g,i);
-    delay(5);
-  }
-  delay(100);
-
-  for(int i = 1024; i >= 0; i--) {
-    analogWrite(r,i);
-    analogWrite(g,i);
-    delay(5);
-  }
-  delay(100);
-
-  // magenta
-  for(int i = 0; i <= 1024; i++) {
-    analogWrite(r,i);
-    analogWrite(b,i);
-    delay(5);
-  }
-  delay(100);
-
-  for(int i = 1024; i >= 0; i--) {
-    analogWrite(r,i);
-    analogWrite(b,i);
-    delay(5);
-  }
-  delay(100);
-
-  // cyan
-    for(int i = 0; i <= 1024; i++) {
-    analogWrite(g,i);
-    analogWrite(b,i);
-    delay(5);
-  }
-  delay(100);
-
-  for(int i = 1024; i >= 0; i--) {
-    analogWrite(g,i);
-    analogWrite(b,i);
-    delay(5);
-  }
-  delay(100);
-
-  // white
-    for(int i = 0; i <= 1024; i++) {
-    analogWrite(r,i);
-    analogWrite(g,i);
-    analogWrite(b,i);
-    delay(5);
-  }
-  delay(100);
-
-  for(int i = 1024; i >= 0; i--) {
-    analogWrite(r,i);
-    analogWrite(g,i);
-    analogWrite(b,i);
-    delay(5);
-  }
-  delay(100);
-
-  // black - no light
-  digitalWrite(r,LOW);
-  digitalWrite(g,LOW);
-  digitalWrite(b,LOW);
-  delay(5320);
-  
 }
